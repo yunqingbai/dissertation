89ee7a8cf115185c1fca666fbdf16c0aed0df770
yunqingbai <72498255+yunqingbai@users.noreply.github.com>
2021-09-21 23:05:59 +0800

    Update ramdom_tree_importance.py

--- two-600/ramdom_tree_importance.py
+++ two-600/ramdom_tree_importance.py
@@ -1,4 +1,6 @@
-#用随机森林来研究特征的重要性
+#Use random forest to study the importance of features
+
+
 
 
 import numpy as np
@@ -12,7 +14,8 @@ from scipy import signal
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn import svm
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import KFold, LeaveOneOut, LeavePOut, ShuffleSplit  # 交叉验证所需的子集划分方法
+from sklearn.model_selection import KFold, LeaveOneOut, LeavePOut, ShuffleSplit  # Subset division method required for cross-validation
+
 import matplotlib as mpl
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
@@ -25,7 +28,8 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import AdaBoostClassifier
 import warnings
 warnings.filterwarnings("ignore")
-# 读正式数据
+
+# Read official data
 import numpy as np
 import pandas as pd
 import random
@@ -56,11 +60,11 @@ pl.rcParams['axes.unicode_minus'] = False
 import warnings
 warnings.filterwarnings("ignore")
 
-# 读训练测试数据
+# Read training test data
 train_set = pd.read_excel("train.xlsx",index_col=0)
 test_set = pd.read_excel("test.xlsx",index_col=0)
 
-# 处理数据到可以进模型
+# Process data until it can be fed into the model
 y_train = list(train_set.iloc[:,0])
 y_test = list(test_set.iloc[:,0])
 
@@ -70,7 +74,7 @@ X_test = np.array(test_set.iloc[:,1:])
 data = np.concatenate((X_train,X_test),axis=0)
 label =y_train.copy()
 label.extend(y_test)
-# 对数据进行归一化标准化
+# Normalize the data
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 
@@ -85,48 +89,45 @@ estimator = RandomForestClassifier(n_estimators=6, random_state=6)
 
 estimator.fit(data,label)
 
-#维度重要性
+#Dimension importance
 feature_label = list(train_set)[1:]
 print(len(feature_label))
 impotances = estimator.feature_importances_
 result = pd.concat([pd.DataFrame(feature_label),pd.DataFrame(impotances)],axis=1)
 result.to_excel("impotances_result.xlsx")
 
-# #画柱状图
+# #Draw histogram
 #
 import matplotlib.pyplot as plt
 import numpy as np
 plt.rcParams['font.family'] = ['sans-serif']
 plt.rcParams['font.sans-serif'] = ['SimHei']
 
-# 创建一个点数为 8 x 6 的窗口, 并设置分辨率为 80像素/每英寸
+# Create a window with 8 x 6 dots and set the resolution to 80 pixels/inch
 plt.figure(figsize=(8, 6), dpi=80)
-# 再创建一个规格为 1 x 1 的子图
+# Create another subgraph with a size of 1 x 1
 plt.subplot(1, 1, 1)
-# 柱子总数
+# Total number of columns
 N = len(feature_label)
-# 包含每个柱子对应值的序列
+# A sequence containing the corresponding value of each bar
 values = impotances
-# 包含每个柱子下标的序列
+# A sequence containing the subscripts of each column
 index = np.arange(N)
-# 柱子的宽度
+# The width of the column
 width = 0.5
-# 绘制柱状图, 每根柱子的颜色为紫罗兰色
+# Draw a histogram, the color of each column is violet
 p2 = plt.bar(index, values, width, label="impotance", color="#87CEFA")
-# 设置横轴标签
+# Set horizontal axis label
 plt.xlabel('feature')
-# 设置纵轴标签
+# Set the vertical axis
 plt.ylabel('importance')
-# 添加标题
 plt.title('feature importance')
-# 添加纵横轴的刻度
 plt.xticks(index, feature_label,rotation=90)
 plt.yticks(np.arange(0, 0.6,0.1))
-# 添加图例
 plt.legend(loc="upper right")
 plt.show()
 
-# 生成随机森林的拟合结果
+# Generate the fitting results of the random forest
 plt.rcParams['font.family'] = ['sans-serif']
 plt.rcParams['font.sans-serif'] = ['SimHei']
 from IPython.display import Image
@@ -147,7 +148,7 @@ for index, model in enumerate(Estimators):
                          filled=True, rounded=True,
                          special_characters=True)
     graph = pydotplus.graph_from_dot_data(dot_data)
-    # 使用ipython的终端jupyter notebook显示。
+    # Use ipython terminal jupyter notebook to display.
     Image(graph.create_png())
     graph.write_png(filename)
 
