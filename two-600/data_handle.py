4d60cfc5b97ccfdd4919856e01b597edc48ebbbe
yunqingbai <72498255+yunqingbai@users.noreply.github.com>
2021-09-21 23:02:27 +0800

    Update data_handle.py

--- two-600/data_handle.py
+++ two-600/data_handle.py
@@ -8,45 +8,46 @@ pl.rcParams['font.sans-serif']=['SimHei']
 pl.rcParams['axes.unicode_minus'] = False
 import warnings
 warnings.filterwarnings("ignore")
-# 调用TSNE降维到三维，然后三维可视化的函数
+
 def plot3D(datayz_true,datayz_true_result):
     import matplotlib.pyplot as plt
     from mpl_toolkits.mplot3d import Axes3D
-    # TSNE降维
-    tsne = TSNE(n_components=3) # TSNE降维
-    normal = pd.DataFrame(tsne.fit_transform(datayz_true))  # 进行数据降维,并返回结果
+    tsne = TSNE(n_components=3) # TSNE dimensionality reduction
+    normal = pd.DataFrame(tsne.fit_transform(datayz_true))  # Perform data dimensionality reduction and return results
 
-    # PCA降维
+
+    # PCA dimensionality reduction
     # PCAE = PCA(n_components=3)
     # normal = pd.DataFrame(PCAE.fit_transform(datayz_true))
     datayz_true_result = pd.DataFrame(datayz_true_result)
 
 
-    ax = plt.subplot(111, projection='3d')  # 创建一个三维的绘图工程
-    #  将数据点分成三部分画，在颜色上有区分度
+    ax = plt.subplot(111, projection='3d')  # Create a three-dimensional drawing project
+    #  Draw the data points in three parts, differentiated in color
     colors = [  'black','red','yellow', 'blue', 'burlywood','blanchedalmond','gold','lightcoral','magenta','navy']
     for i in range(len(colors)):
         temp = normal[datayz_true_result[0] == i]
         x, y, z = temp[0], temp[1], temp[2]
-        ax.scatter(x, y, z, c=colors[i])  # 绘制数据点
-    ax.set_zlabel('Z')  # 坐标轴
+        ax.scatter(x, y, z, c=colors[i])  # Plotting data points
+    ax.set_zlabel('Z')  # Coordinate axes
     ax.set_ylabel('Y')
     ax.set_xlabel('X')
     plt.show()
 
-# 读数据并进行数据处理
+# Read data and perform data
 data = pd.read_excel("data.xlsx")
 data_index = list(data)
 data_index_chara = data_index[1:]
 print(data)
 print(data_index)
 
-# 匹配道路类'
+# Matching road class'
 street_type = ['Rd','Dr', 'St','Ave','N','S','E','W','Blvd','Ln','Highway','Way','Pkwy','Hwy','Ct','SW','NE','Pl','NW','State',
                'Old','SE','Road','Cir','US','Creek','County','Hill','Park','Route','Lake','Trl','I','Valley','Ridge','Mill',
                'River','Oak','Pike','Loop']
 
-# 匹配类型--Description和Street -- 注意解决上面的关键词的大小写的方案--采用将所有字符都转换为小写进行匹配的方式
+
+# Matching Type - Description and Street - Note Solution to Solution Size Size - Use the way to convert all characters to lowercase matches
 for i in range(len(data['Description'])):
     temp = data['Description'][i].lower().strip().split()
     data['Description'][i] = 0
@@ -62,9 +63,9 @@ for i in range(len(data['Street'])):
             data['Street'][i] = j
             break
 
-# 重置index
+# Reset the index
 data = data.reset_index(drop=True)
-# 对基本唯一的某列列进行离散值处理
+# Discrete value processing for a column that is essentially unique
 disp_list = ['Description', 'Street', 'Side', 'Borough','Weather_Condition', 'Civil_Twilight','Amenity', 'Bump', 'Crossing', 'Give_Way', 'Junction',
              'No_Exit', 'Railway', 'Roundabout', 'Station', 'Stop', 'Traffic_Calming', 'Traffic_Signal', 'Turning_Loop']
 disp_dict = {}
@@ -75,7 +76,7 @@ for i in disp_list:
         temp_dict[disp_set[j]] = j
     disp_dict[i] = temp_dict
 
-# 通过上述的字典进行离散化处理-即替换
+# Discretization by the above dictionary - i.e. replacement
 for i in disp_list:
     temp_dict = disp_dict[i]
     # print(temp_dict)
@@ -83,7 +84,7 @@ for i in disp_list:
         # print(k,v)
         data[i] = data[i].replace(k, v)
 
-# 将所有空值先用0填充
+# Fill all null values with zeros first
 data.iloc[:,1:] = data.iloc[:,1:].fillna(0)
 data.iloc[:,1:] = data.iloc[:,1:].replace(np.nan, 0)
 # data.iloc[:,1:] = data.iloc[:,1:].replace(0, "")
@@ -91,21 +92,21 @@ data.iloc[:,1:] = data.iloc[:,1:].replace(np.nan, 0)
 # data.iloc[:,1:] = data.iloc[:,1:].replace(0, "\t")
 data.iloc[:,0] = data.iloc[:,0].fillna(3)
 
-# 输出处理后的整体数据
+# Output processed overall data
 data.to_excel("data_handle.xlsx")
 print(data)
-# 画图
+# Drawing
 plot3D(np.array(data.iloc[:,1:]),np.array(data.iloc[:,0]))
 
 
-# 划分训练集和测试集
+#Divide training set and test set
 data_select_2 = np.array(data)
 import random
 random.shuffle(data_select_2)
 from sklearn.model_selection import train_test_split
-#data:需要进行分割的数据集
-#random_state:设置随机种子，保证每次运行生成相同的随机数
-#test_size:将数据分割成训练集的比例7：3
+#data: The data set that needs to be split
+#random_state: Set the random seed to ensure that the same random number is generated every time you run
+#test_size: The ratio of dividing the data into the training set 7:3
 train_set, test_set = train_test_split(data_select_2, test_size=0.3, random_state=42)
 train_set = pd.DataFrame(train_set)
 test_set = pd.DataFrame(test_set)
